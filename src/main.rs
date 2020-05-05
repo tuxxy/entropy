619dc3f9e8fc594360aa59a961d1c4f78e375a48
tuxxy <me@johnpacific.com>
2020-05-05 18:50:22 +0200

    Add CLI flow

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use clap::Clap;
+use entropy::Entropy;
+use std::fs::File;
 
 #[derive(Clap)]
 #[clap(
     version = "0.1",
     author = "tux <me@johnpacific.com>",
-    about = "A utility to calculate Shannon entropy"
+    about = "A utility to calculate Shannon entropy of a given file"
 )]
 struct Opts {
-    filename: String,
+    #[clap(about = "The target file to measure")]
+    filepath: String,
+    #[clap(short, long, about = "Returns metric entropy instead of Shannon entropy")]
+    metric_entropy: bool,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
+
+    let file = File::open(opts.filepath).expect("Couldn't open the file at the provided path.");
+    let entropy_calc = Entropy::new(&file);
+
+    // --metric-entropy
+    if opts.metric_entropy {
+        println!("{}", entropy_calc.metric_entropy());
+    } else {
+        println!("{}", entropy_calc.shannon_entropy());
+    }
 }
